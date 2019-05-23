5c2d8bba97d62cd4da09135088f5f8eb77a2cb78
Abijit Biswas <abhijitb.2204@gmail.com>
2019-05-24 00:12:44 +0530

    promise issue fixed

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const axios = require('axios');
 
-function getIFSC(ifsc) {
-
-    let url = `https://ifsc.razorpay.com/${ifsc}`
-
-    axios.get(url)
-        .then((response) => {
-            console.log("BANK DETAILS", response.data);
-        })
-        .catch((error) => {
-            console.log("ERROR node-ifsc", error);
-        })
+async function getIFSC(ifsc) {
+	let url = `https://ifsc.razorpay.com/${ifsc}`;
+	let { data } = await axios.get(url);
+	return data;
 }
+
 module.exports = getIFSC;
